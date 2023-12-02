63eae0ba316c48064e77f95af62c34f6ba17faa3
unknown <dineshj405@gmail.com>
2023-12-02 14:50:47 +0530

    CI-CD updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 
 @app.route("/",methods['GET','POST'])
 def undex():
-    return "Starting Machine Learning Project"
+    return "CI-CD pipeline has been established"
 
 
 if __name__=="__main__":
